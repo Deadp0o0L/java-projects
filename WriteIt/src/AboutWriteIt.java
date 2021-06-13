85fb1cbc3635e67d7c8aaeb92121b3b18d79931c
Dheeraj Kumar <dhiraj2797@gmail.com>
2021-06-13 18:02:54 +0530

    v2.1

--- WriteIt/src/AboutWriteIt.java
+++ WriteIt/src/AboutWriteIt.java
@@ -15,25 +15,25 @@ public class AboutWriteIt implements ActionListener {
 	
 	
 	l1 = new JLabel("WriteIt");
-	l1.setBounds(150,20,75,30);
+	l1.setBounds(190,20,75,30);
 	l1.setFont(new Font("veranda",Font.ITALIC,25));
 	af.add(l1);
 	
-	l2 = new JLabel(String.format("<html><body style=\"text-align: justify; text-justify:inter-word;\" >%s</body></html>","Credit to Hina Kausar for the name of the Application. Also I would like to thanks Mr. Engimachii Niraj K Gupta for the valuable inputs."));
-	l2.setBounds(5,60,370,100);
+	l2 = new JLabel(String.format("<html><body style=\"text-align: justify; text-justify:inter-word;\" >%s</body></html>","WriteIt is Java based application which comes with various functionalities including saving the file as text or pdf , changing the backgroud and font as well.<br><br>Credit to Hina Kausar for the name of the Application. <br> Also I would like to thanks Mr. Engimachii Niraj K Gupta for the valuable inputs. <br><br> NEIL © COPYRIGHT "));
+	l2.setBounds(5,60,470,200);
 	l2.setFont(new Font("veranda",Font.ITALIC,15));
 	af.add(l2);
 	
 	b = new JButton("Back");
 	b.setBackground(Color.black);
 	b.setForeground(Color.white);
-	b.setBounds(150,200,75,25);
+	b.setBounds(200,250,75,25);
 	b.addActionListener(this);
 	af.add(b);
 	
 	af.getContentPane().setBackground(Color.white);
 	af.setVisible(true);
-	af.setSize(400,300);
+	af.setSize(500,400);
 	af.setLocation(400,200);
 		
 	}
