528d3a42ecae79743e59fbda3ac61717f691cfd5
Dheeraj Kumar <dhiraj2797@gmail.com>
2021-06-13 17:42:50 +0530

    WriteIt v2.0

--- WriteIt/src/AboutWriteIt.java
+++ WriteIt/src/AboutWriteIt.java
@@ -19,7 +19,7 @@ public class AboutWriteIt implements ActionListener {
 	l1.setFont(new Font("veranda",Font.ITALIC,25));
 	af.add(l1);
 	
-	l2 = new JLabel(String.format("<html><body style=\"text-align: justify; text-justify:inter-word;\" >%s</body></html>","Credit to Hina Kausar for the name of the Application. Also I would like to thanks Mr. Engimachii Niraj Kr. Gupta for the valuable inputs."));
+	l2 = new JLabel(String.format("<html><body style=\"text-align: justify; text-justify:inter-word;\" >%s</body></html>","Credit to Hina Kausar for the name of the Application. Also I would like to thanks Mr. Engimachii Niraj K Gupta for the valuable inputs."));
 	l2.setBounds(5,60,370,100);
 	l2.setFont(new Font("veranda",Font.ITALIC,15));
 	af.add(l2);
--- WriteIt/src/WriteIt.java
+++ WriteIt/src/WriteIt.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import java.awt.*;
+import java.awt.Font;
 import java.awt.event.*;
 import java.io.*;
 
@@ -8,13 +13,14 @@ public class WriteIt implements ActionListener {
 	JFrame f;
 	JPanel p;
 	JMenuBar mb;
-	JMenu m1, m2, m3;
+	JMenu m1, m2, m3, m4;
 	JTextField t;
 	JTextArea a;
 	JScrollPane scr;
-	JMenuItem m11, m12, m13, m14, m21, m22, m23, m24, m31;
+	JMenuItem m11, m12, m13, m14, m15, m21, m22, m23, m24, m25, m31, m32, m33, m34, m35, m41;
 	JLabel l1;
 	static String str;
+	String Title;
 
 	WriteIt() {
 
@@ -37,11 +43,14 @@ public class WriteIt implements ActionListener {
 		m13 = new JMenuItem("Save As");
 		m13.addActionListener(this);
 		m1.add(m13);
-		m14 = new JMenuItem("Exit");
+		m14 = new JMenuItem("Save As PDF");
 		m14.addActionListener(this);
 		m1.add(m14);
+		m15 = new JMenuItem("Exit");
+		m15.addActionListener(this);
+		m1.add(m15);
 
-		m2 = new JMenu("Change Color");
+		m2 = new JMenu("Color");
 		mb.add(m2);
 
 		m21 = new JMenuItem("Red");
@@ -56,30 +65,79 @@ public class WriteIt implements ActionListener {
 		m24 = new JMenuItem("Green");
 		m24.addActionListener(this);
 		m2.add(m24);
+		m25 = new JMenuItem("Default");
+		m25.addActionListener(this);
+		m2.add(m25);
 
-		m3 = new JMenu("Help");
+		m3 = new JMenu("Font");
 		mb.add(m3);
 
-		m31 = new JMenuItem("About WriteIt");
+		m31 = new JMenuItem("Serrif");
 		m31.addActionListener(this);
 		m3.add(m31);
+		m32 = new JMenuItem("MonoSpaced");
+		m32.addActionListener(this);
+		m3.add(m32);
+		m33 = new JMenuItem("Verdana");
+		m33.addActionListener(this);
+		m3.add(m33);
+		m34 = new JMenuItem("Helvetica");
+		m34.addActionListener(this);
+		m3.add(m34);
+		m35 = new JMenuItem("Default");
+		m35.addActionListener(this);
+		m3.add(m35);
+
+		m4 = new JMenu("Help");
+		mb.add(m4);
+
+		m41 = new JMenuItem("About WriteIt");
+		m41.addActionListener(this);
+		m4.add(m41);
 
 		p = new JPanel();
 		p.setBounds(0, 20, 700, 580);
 
 		a = new JTextArea();
 		a.setLineWrap(true);
-		a.setFont(new Font("Sansserrif", Font.BOLD+Font.ITALIC,15));
+		a.setFont(new Font("Sansserrif", Font.BOLD + Font.ITALIC, 15));
 		scr = new JScrollPane(a);
 		scr.setBounds(0, 20, 700, 580);
 		f.add(scr);
 
 		f.add(p);
-				
 
 		f.setVisible(true);
 		f.setSize(700, 600);
 		f.setLocation(300, 100);
+
+		f.addWindowStateListener(new WindowAdapter() {
+
+			public void windowStateChanged(WindowEvent w) {
+				int newState = w.getNewState();
+				int oldState = w.getOldState();
+
+				if ((oldState & JFrame.MAXIMIZED_BOTH) == 0 && (newState & JFrame.MAXIMIZED_BOTH) != 0) {
+					mb.setBounds(0, 0, 1366, 20);
+					p.setBounds(0, 20, 1366, 718);
+					scr.setBounds(0, 20, 1366, 718);
+					f.add(scr);
+					f.add(p);
+				}
+
+				if ((oldState & JFrame.MAXIMIZED_BOTH) != 0 && (newState & JFrame.MAXIMIZED_BOTH) == 0) {
+					mb.setBounds(0, 0, 700, 20);
+					p.setBounds(0, 20, 700, 580);
+					scr.setBounds(0, 20, 700, 580);
+					f.add(scr);
+					f.add(p);
+				}
+
+			}
+
+		});
+
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 	}
 
@@ -87,7 +145,7 @@ public class WriteIt implements ActionListener {
 
 		str = a.getText();
 
-		if (ee.getSource() == m31) {
+		if (ee.getSource() == m41) {
 			new AboutWriteIt();
 		}
 
@@ -101,29 +159,86 @@ public class WriteIt implements ActionListener {
 		}
 
 		if (ee.getSource() == m13) {
-			new SaveAs();
+			selectFile(str);
+
 		}
 
 		if (ee.getSource() == m14) {
+			try {
+				SaveAsPDF(str);
+			} catch (Exception e) {
+			}
+		}
+
+		if (ee.getSource() == m15) {
 			System.exit(0);
 		}
-		
+
 		if (ee.getSource() == m21) {
 			a.setBackground(Color.red);
 		}
-		
+
 		if (ee.getSource() == m22) {
 			a.setBackground(Color.blue);
 		}
-		
+
 		if (ee.getSource() == m23) {
 			a.setBackground(Color.decode("#FFA500"));
 		}
-		
+
 		if (ee.getSource() == m24) {
 			a.setBackground(Color.green);
 		}
+		
+		if (ee.getSource() == m25) {
+			a.setBackground(Color.white);
+		}
 
+		if (ee.getSource() == m31) {
+			a.setFont(new Font("serrif", Font.PLAIN, 15));
+		}
+
+		if (ee.getSource() == m32) {
+			a.setFont(new Font("monospaced", Font.PLAIN, 15));
+		}
+
+		if (ee.getSource() == m33) {
+			a.setFont(new Font("Verdana", Font.PLAIN, 15));
+		}
+
+		if (ee.getSource() == m34) {
+			a.setFont(new Font("Helvetica", Font.PLAIN, 15));
+		}
+
+		if (ee.getSource() == m35) {
+			a.setFont(new Font("Sansserrif", Font.BOLD + Font.ITALIC, 15));
+		}
+
+	}
+
+	public void SaveAsPDF(String content) throws Exception {
+
+		try {
+
+			JFileChooser filechooser = new JFileChooser();
+
+			int option = filechooser.showSaveDialog(null);
+
+			if (option == JFileChooser.APPROVE_OPTION) {
+				File file = filechooser.getSelectedFile();
+
+				Document doc = new Document();
+				PdfWriter.getInstance(doc, new FileOutputStream(file + ".pdf"));
+
+				doc.open();
+				Paragraph para = new Paragraph(content);
+				doc.add(para);
+				doc.close();
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void Save(String s) {
@@ -139,6 +254,33 @@ public class WriteIt implements ActionListener {
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+	}
+
+	public void selectFile(String content) {
+
+		JFileChooser filechooser = new JFileChooser();
+
+		int option = filechooser.showSaveDialog(null);
+
+		if (option == JFileChooser.APPROVE_OPTION) {
+			File file = filechooser.getSelectedFile();
+
+			f.setTitle("WriteIt | " + file.getName());
+
+			try {
+
+				FileWriter fw = new FileWriter(file + ".txt");
+				BufferedWriter bw = new BufferedWriter(fw);
+				bw.write(content);
+				bw.flush();
+				bw.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
